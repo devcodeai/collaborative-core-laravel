73bf7772bed52e84ea7c186f78c56b02f1ce9960
ravaattar <rava.naufal2014@gmail.com>
2023-08-08 14:12:21 +0700

    add: database create tables migration

--- src/database/migrations/2023_08_08_070259_create_talents_table.php
+++ src/database/migrations/2023_08_08_070259_create_talents_table.php
@@ -9,24 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('talents', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('email');
+            $table->string('skills');
         });
-    }
+    }    
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('talents');
     }
 };
