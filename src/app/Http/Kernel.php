7bca4f82432ec665d67a41b2d29f095ba4f2d08d
Suryanto Tan <suryanto.tan5@gmail.com>
2023-08-09 10:46:40 +0700

    add throttle to limit 1000 req per minute

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -39,8 +39,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'throttle:1000,1', // Allow 100 requests per minute
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
